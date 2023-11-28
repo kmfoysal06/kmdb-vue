1d47bb657bc8d87cc7e2e3f487196136820b1dee
kazi mohammed foysal <foysalkazimd01@gmail.com>
2023-11-28 20:07:03 +0600

    removed live link for some reson

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@
 </template>
 
 <script>
-import './assets/css/main.css'
+import './assets/styles/main.css'
 export default{
   name:"HomeView",
   components:{
--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router'
 import Gstore from './store'
 import 'nprogress/nprogress.css'
+import "@/assets/styles/main.css";
 
 createApp(App).use(router).provide('Gstore',Gstore).mount('#app')
