d05995bb6d4b3e6538da4bb3579ffa14c9849ac8
kazi mohammed foysal <foysalkazimd01@gmail.com>
2023-11-28 18:02:10 +0600

    installed tailwindcss cli

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+                primary: '#FF1F3D',
+              }
+    },
   },
   plugins: [],
 }
