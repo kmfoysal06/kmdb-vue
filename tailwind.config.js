2645be60aa55f1ce0a869428f94268715e505ea0
kazi mohammed foysal <foysalkazimd01@gmail.com>
2023-11-29 20:07:03 +0600

    updated tailwindcss theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./public/index.html", "./src/**/*.{vue,js,ts,jsx,tsx}", ],
     theme: {
-        extend: {},
+        extend: {
+          colors: {
+                primary: '#FF1F3D',
+              }
+        },
     },
     plugins: [require("daisyui")],
 }
